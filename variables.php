9b9c5a1d22398137b2ad9644d20232019ab9b75d
clariton <69374779+githubkurry@users.noreply.github.com>
2021-01-30 10:27:36 +0900

    Update variables.php

--- variables.php
+++ variables.php
@@ -1,8 +1,25 @@
 <?php
-$x=5;
+$x=5;  //全局变量，函数内不能使用
 $y=10;
 $z=$x*$y;
 $txt="hello";
 echo $z;
 echo $txt;
+echo "<br>";
+echo $a;
+echo "<br>";
+
+function test(){
+    $a=6;//局部变量
+    echo $a;
+    echo $x;
+}
+test();
+echo "<br>";
+
+function test2(){
+    global $x;//global以后的全局变量可在函数内使用
+    echo $x;
+}
+test2();
 ?>
