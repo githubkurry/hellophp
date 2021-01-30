1c30a4fe9a3329fd73e8b0e883f059549a32328e
clariton <69374779+githubkurry@users.noreply.github.com>
2021-01-30 21:13:54 +0900

    Create echo.php

---
+++ echo.php
@@ -0,0 +1,11 @@
+<?php
+$txt1="做汉堡";
+$txt2="无聊";
+$hamburg=array("レモンタツタ","エビフィレオ","KitKatアイス","ポテト");
+
+echo "今日はバイトの一日、$txt1 はとても $txt2 です。";
+echo "私一番好きなバーガーは {$hamburg[1]} です",",","{$hamburg[3]}も好きです";
+echo "<br>";
+print "今日はバイトの一日、$txt1 はとても $txt2 です。";
+print "私一番好きなバーガーは {$hamburg[1]} です";
+?>
