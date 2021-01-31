16193bb8ac40925bb1720fa05d9ada4b5b2fffdb
clariton <69374779+githubkurry@users.noreply.github.com>
2021-01-31 10:31:32 +0900

    Update echo.php

--- echo.php
+++ echo.php
@@ -16,5 +16,55 @@ echo <<<EOF
       $txt1 有意思
       haha
 EOF;
+echo "<br>";
 
+
+//define常量
+define("uki","got a job",false);//(name,value,case insensitive）
+echo uki;
+echo "<br>";
+function test5(){//全局变量
+    echo uki;
+}
+test5();
+echo "<br>";
+
+//并置运算符
+echo $txt1." ".$txt2;
+echo "<br>";
+echo $txt1.$txt2;
+echo "<br>";
+
+//strlen()
+echo strlen($txt1);
+echo "<br>";
+echo strlen($txt1);
+echo "<br>";
+echo strlen($txt2);
+echo "<br>";
+echo strlen($hamburg[1]);
+echo "<br>";
+
+//strops()
+echo strpos($txt1,"汉");
+
+//== ===类型比较
+$txt3=66;
+$txt4="66";
+if($txt3 == $txt4){
+    echo "值相等";
+}
+echo PHP_EOL;
+
+if($txt3 === $txt4){
+    echo "类型相等";
+}else{
+    echo "类型不相等";
+}
+
+//运算符号
+$x=5;
+$y=50;
+echo ($x - $y)." ".($x * $y)." ".($x + $y)." ".($x / $y)." ".($y % $x);
+echo $txt1.=$txt2;
 ?>
