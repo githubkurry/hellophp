efbc03d03d6572857e55a0482480bddb6e94fa2c
clariton <69374779+githubkurry@users.noreply.github.com>
2021-02-05 10:39:59 +0900

    Create multiarray.php

---
+++ multiarray.php
@@ -0,0 +1,11 @@
+<?php
+$idol = array(
+    "bp" => array(4,1),
+    "red velvet" => array(5,2),
+    "izone" => array(11,3)
+);
+print("<pre>");//格式化输出数组
+print_r($idol);
+print("<pre>");
+echo $idol['bp'][0];
+?>
