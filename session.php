eb7dc84cb6007691647c44fe902be7965050e310
clariton <69374779+githubkurry@users.noreply.github.com>
2021-02-07 00:00:49 +0900

    Create session.php

---
+++ session.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+ 
+if(isset($_SESSION['views']))
+{
+    $_SESSION['views']=$_SESSION['views']+1;
+}
+else
+{
+    $_SESSION['views']=1;
+}
+echo "浏览量：". $_SESSION['views'];
+
+if(isset($_SESSION['views']))
+{
+    unset($_SESSION['views']);
+}
+?>
